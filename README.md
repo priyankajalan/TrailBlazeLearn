eaa124df508538cc479d3acb4c6f9f261257998a
sourcepirate <plasmashadowx@gmail.com>
2018-03-03 14:40:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## TrailBlazeLearn
+
+[![codecov](https://codecov.io/gh/NUSBigHero6/TrailBlazeLearn/branch/master/graph/badge.svg)](https://codecov.io/gh/NUSBigHero6/TrailBlazeLearn)
+[![Build Status](https://travis-ci.org/NUSBigHero6/TrailBlazeLearn.svg?branch=master)](https://travis-ci.org/NUSBigHero6/TrailBlazeLearn)
---
+++ app/src/main/java/org/nus/trailblaze/TrailBlazeMainActivity.java
@@ -0,0 +1,13 @@
+package org.nus.trailblaze;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrailBlazeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trail_blaze_main);
+    }
+}
