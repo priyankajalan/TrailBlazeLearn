2560d05bfd3fa9e75d734b4cbfba56bd3f76aeca
JALAN\priyanka <priyankajalan1991@gmail.com>
2018-03-16 01:04:25 +0800

    New Learning Trail UI form component (Textbox and Button)

--- app/src/main/java/org/nus/trailblaze/views/LoginActivity.java
+++
@@ -1,8 +0,0 @@
-package org.nus.trailblaze.views;
-
-/**
- * Created by priyanka on 15/3/2018.
- */
-
-public class LoginActivity {
-}
---
+++ app/src/main/java/org/nus/trailblaze/views/NewTrailActivity.java
@@ -0,0 +1,18 @@
+package org.nus.trailblaze.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import org.nus.trailblaze.R;
+
+/**
+ * Created by priyanka on 15/3/2018.
+ */
+
+public class NewTrailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_learning_trail);
+    }
+}
