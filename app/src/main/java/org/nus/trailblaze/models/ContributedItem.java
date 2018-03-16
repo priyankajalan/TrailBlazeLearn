d1a6ed74f3751423d71cd579ebd9f601b4863edf
Chen Weng Wei <wwchen21227@gmail.com>
2018-03-16 23:23:28 +0800

    chore(app/src/main) implements Parcelable

--- app/src/main/java/org/nus/trailblaze/models/ContributedItem.java
+++ app/src/main/java/org/nus/trailblaze/models/ContributedItem.java
@@ -1,6 +1,8 @@
 package org.nus.trailblaze.models;
 
 import android.media.Image;
+import android.os.Parcel;
+import android.os.Parcelable;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -25,5 +27,36 @@ public class ContributedItem extends Item {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    // Storing the Movie data to Parcel object
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getId());
+        dest.writeString(getDescription());
+        dest.writeParcelable(getFile(), flags);
+    }
+
+    /**
+     * Retrieving Movie data from Parcel object
+     * This constructor is invoked by the method createFromParcel(Parcel source) of
+     * the object CREATOR
+     **/
+    private ContributedItem(Parcel in){
+        this.setId(in.readString());
+        this.setDescription(in.readString());
+        this.setFile((File)in.readParcelable(File.class.getClassLoader()));
+    }
+
+    public static final Parcelable.Creator<ContributedItem> CREATOR = new Parcelable.Creator<ContributedItem>() {
+        @Override
+        public ContributedItem createFromParcel(Parcel source) {
+            return new ContributedItem(source);
+        }
+
+        @Override
+        public ContributedItem[] newArray(int size) {
+            return new ContributedItem[size];
+        }
+    };
 }
 
--- app/src/main/java/org/nus/trailblaze/models/Item.java
+++ app/src/main/java/org/nus/trailblaze/models/Item.java
@@ -3,13 +3,18 @@ package org.nus.trailblaze.models;
 /**
  * Created by plasmashadow on 3/11/18.
  */
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
-public class Item {
+public class Item implements Parcelable {
     private String id;
     private User user;
     private Date createdDate;
     private File file;
+
+    public Item(){}
 
     public Item(String id, User user, Date createdDate, File file) {
         this.id = id;
@@ -49,4 +54,36 @@ public class Item {
     public void setFile(File file) {
         this.file = file;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    // Storing the Movie data to Parcel object
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(getId());
+    }
+
+    /**
+     * Retrieving Movie data from Parcel object
+     * This constructor is invoked by the method createFromParcel(Parcel source) of
+     * the object CREATOR
+     **/
+    private Item(Parcel in){
+        this.setId(in.readString());
+    }
+
+    public static final Parcelable.Creator<Item> CREATOR = new Parcelable.Creator<Item>() {
+        @Override
+        public Item createFromParcel(Parcel source) {
+            return new Item(source);
+        }
+
+        @Override
+        public Item[] newArray(int size) {
+            return new Item[size];
+        }
+    };
 }
