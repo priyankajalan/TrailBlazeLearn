d5a63e549a3d97e06d5dc48e0abcaccc2f602ee8
LC1986 <liucao1984@hotmail.com>
2018-03-24 12:31:44 +0800

    Update changes

---
+++ app/src/main/java/org/nus/trailblaze/views/ContributedItemImageActivity.java
@@ -0,0 +1,15 @@
+package org.nus.trailblaze.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.nus.trailblaze.R;
+
+public class ContributedItemImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contributed_item_image);
+    }
+}
---
+++ app/src/main/java/org/nus/trailblaze/views/ContributedItemMediaActivity.java
@@ -0,0 +1,15 @@
+package org.nus.trailblaze.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.nus.trailblaze.R;
+
+public class ContributedItemMediaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contributed_item_media);
+    }
+}
