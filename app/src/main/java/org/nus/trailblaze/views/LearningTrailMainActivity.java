4593424b848a3476d35e737ae47b5414ad13de07
Chen Weng Wei <wwchen21227@gmail.com>
2018-03-27 22:28:00 +0800

    Chore (app) Wired up Add learning trail button.

--- app/src/main/java/org/nus/trailblaze/views/LearningTrailMainActivity.java
+++ app/src/main/java/org/nus/trailblaze/views/LearningTrailMainActivity.java
@@ -1,35 +1,56 @@
 package org.nus.trailblaze.views;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
 import org.nus.trailblaze.R;
 import org.nus.trailblaze.adapters.LearningTrailFirestoreAdaptor;
 import org.nus.trailblaze.listeners.ListItemClickListener;
+import org.nus.trailblaze.listeners.SignInFailureListener;
+import org.nus.trailblaze.listeners.SignInListener;
 import org.nus.trailblaze.models.LearningTrail;
 
 public class LearningTrailMainActivity extends Activity implements ListItemClickListener {
 
     private static final String TAG = "LearningTrailActivity";
+    public static final int CREATE_NEW_TRAIL = 1000;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.LayoutManager mLayoutManager;
     private FirestoreRecyclerAdapter adapter;
     private FirebaseFirestore firestoreDB;
 
+    private Button btnAddLearningTrail;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learning_trail_main);
         mRecyclerView = (RecyclerView) findViewById(R.id.rvLearningTrail);
+        btnAddLearningTrail = (Button) findViewById(R.id.btnAddLearningTrail);
+        btnAddLearningTrail.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LearningTrailMainActivity.this, SetLearningTrailActivity.class);
+                startActivity(intent); // startActivity allow you to move
+            }
+        });
 
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
@@ -107,4 +128,5 @@ public class LearningTrailMainActivity extends Activity implements ListItemClick
 //            buttonOptions = (Button) itemView.findViewById(R.id.buttonOptions);
 //        }
 //    }
+
 }
